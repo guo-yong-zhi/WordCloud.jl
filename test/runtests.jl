6a19eb38a4850209b64a8a35f446f9efbec9604c
guoyongzhi <guoyongzhi@sensetime.com>
2020-12-28 11:29:56 +0800

    fix process arg type

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,7 @@ include("test_lru.jl")
 end
 
 @testset "WordCloud.jl" begin
+    # @show pwd()
     img, img_m = WordCloud.rendertext("test", 88.3, color="blue", angle = 20, border=1, returnmask=true)
     words = [Random.randstring(rand(1:8)) for i in 1:rand(100:1000)]
     weights = randexp(length(words)) .* 1000 .+ randexp(length(words)) .* 200 .+ rand(20:100, length(words));
@@ -30,5 +31,11 @@ end
     wordcloud(process("giving a single word is ok. giving several words is ok too"), 
             maskimg=shape(box, 20, 15, 0, color=0.15), filling_rate=0.5, transparentcolor=(1,1,1,0)) #String & small mask
     placement!(wc)
+    wc = wordcloud(
+            process(open("../res/alice.txt"), stopwords=WordCloud.stopwords_en ∪ ["said"]), 
+            mask = loadmask("../res/alice_mask.png", color="#faeef8", backgroundcolor=0.97),
+            colors = (WordCloud.colorschemes[:Set1_5].colors..., ),
+            angles = (0, 90),
+            filling_rate = 0.55);
 end
 
