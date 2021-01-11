0f0a83d3b53b2c56f908339de87953db2937ba83
guoyongzhi <guoyongzhi@sensetime.com>
2021-01-11 20:28:51 +0800

    outline & more examples

---
+++ examples/qianziwen.jl
@@ -0,0 +1,6 @@
+using WordCloud
+words = "天地玄黄宇宙洪荒日月盈昃辰宿列张寒来暑往秋收冬藏闰余成岁律吕调阳云腾致雨露结为霜金生丽水玉出昆冈剑号巨阙珠称夜光果珍李柰菜重芥姜海咸河淡鳞潜羽翔龙师火帝鸟官人皇始制文字乃服衣裳推位让国有虞陶唐吊民伐罪周发殷汤坐朝问道垂拱平章"
+words = [string(c) for c in words]
+weights = rand(length(words)) .^ 2 .* 100 .+ 30
+wc = wordcloud(words, weights)
+generate!(wc)
