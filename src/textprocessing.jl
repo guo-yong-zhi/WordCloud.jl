959bc655a4a3daec0ddc297309578eb274a2df69
guoyongzhi <guoyongzhi@sensetime.com>
2021-02-24 20:46:37 +0800

    simple lemmatization

--- src/textprocessing.jl
+++ src/textprocessing.jl
@@ -4,9 +4,37 @@ dir = @__DIR__
 stopwords_en = Set(readlines(dir * "/../res/stopwords_en.txt"))
 stopwords_cn = Set(readlines(dir * "/../res/stopwords_cn.txt"))
 stopwords = stopwords_en ∪ stopwords_cn
-function splitwords(text::AbstractString, regexp=r"\w[\w']+")
+include("wordlists.jl")
+function lemmatize(word)
+    w = lowercase(word)
+    if (!endswith(w, "s")) || endswith(w, "ss") || w in s_ending_words || uppercase(word)==word
+        return word
+    end
+    if endswith(w, "ies") && !(w[1:prevind(w, end, 1)] in xe_ending_words)
+        return word[1:prevind(word, end, 3)] * "y"
+    end
+    if endswith(w, r"ses|xes|ches|shes|oes") && !(w[1:prevind(w, end, 1)] in xe_ending_words)
+        return word[1:prevind(word, end, 2)]
+    end
+    if endswith(w, "ves")
+        wh = w[1:prevind(w, end, 3)]
+        wordh = word[1:prevind(word, end, 3)]
+        if wh * "fe" in f_ending_words
+            return wordh * "fe"
+        elseif wh * "f" in f_ending_words
+            return wordh * "f"
+        end
+    end
+    return word[1:prevind(word, end, 1)]
+end
+
+function splitwords(text::AbstractString, regexp=r"\w[\w']+"; lemmatizer=lemmatize)
+    if lemmatizer == false || lemmatizer === nothing
+        lemmatizer = x -> x
+    end
     words = findall(regexp, text)
-    words = [endswith(text[i], "'s") ? text[i][1:end - 2] : text[i] for i in words]
+    words = [endswith(text[i], "'s") ? text[i][1:prevind(text[i], end, 2)] : text[i] for i in words]
+    lemmatizer.(words)
 end
 
 function countwords(words::AbstractVector{<:AbstractString}; regexp=r"\w[\w']+", counter=Dict{String,Int}())
@@ -20,12 +48,15 @@ function countwords(words::AbstractVector{<:AbstractString}; regexp=r"\w[\w']+",
     counter
 end
 
-countwords(text::AbstractString; regexp=r"\w[\w']+", kargs...) = countwords(splitwords(text, regexp); kargs...)
+function countwords(text::AbstractString; regexp=r"\w[\w']+", lemmatizer=lemmatize, kargs...)
+    countwords(splitwords(text, regexp, lemmatizer=lemmatizer); kargs...)
+end
 
 raw"""
-countwords(text; regexp=r"\w[\w']+", counter=Dict{String,Int}(), kargs...)
+countwords(text; regexp=r"\w[\w']+", lemmatizer=lemmatize, counter=Dict{String,Int}(), kargs...)
 Count words in text. And use `regexp` to split. And save results in `counter`. 
-`text` can be a String, a Vector of String, or a opend file(IO)
+`lemmatizer` can be `nothing` or a function to do lemmatization.
+`text` can be a String, a Vector of String, or a opend file(IO).
 """
 function countwords(textfile::IO; counter=Dict{String,Int}(), kargs...)
     for l in eachline(textfile)
@@ -93,8 +124,8 @@ function processtext(counter::Dict{<:AbstractString, <:Number};
     words, weights
 end
 
-function processtext(text; regexp=r"\w[\w']+", counter=Dict{String,Int}(), kargs...)
-    processtext(countwords(text, regexp=regexp, counter=counter); kargs...)
+function processtext(text; regexp=r"\w[\w']+", lemmatizer=lemmatize, counter=Dict{String,Int}(), kargs...)
+    processtext(countwords(text, regexp=regexp, lemmatizer=lemmatizer, counter=counter); kargs...)
 end
 processtext(fun::Function; kargs...) = processtext(fun(); kargs...)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,8 @@ using Random
 include("test_qtree.jl")
 include("test_trainer.jl")
 include("test_render.jl")
+include("test_textprocessing.jl")
 
-@testset "textprocessing.jl" begin
-    text = "So dim, so dark, So dense, so dull, So damp, so dank, So dead! The weather, now warm, now cold, Makes it harder Than ever to forget!"
-    c = WordCloud.TextProcessing.countwords(text)
-    @test c["so"] == 3
-    words,weights = WordCloud.TextProcessing.processtext(c)
-    @test !("So" in words)
-end
 
 @testset "WordCloud.jl" begin
     # @show pwd()
