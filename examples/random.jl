1e4a143a3cb85cc6a10e6a7c9a402d7853bed494
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2021-03-28 22:46:09 +0800

    lower random-example's difficulty for CI

--- examples/random.jl
+++ examples/random.jl
@@ -1,6 +1,9 @@
 using WordCloud
 using Random
 
-words = [Random.randstring(rand(1:8)) for i in 1:500]
+words = [Random.randstring(rand(1:8)) for i in 1:300]
 weights = randexp(length(words)) .* 2000 .+ rand(20:100, length(words));
-wc = wordcloud(words, weights, mask=shape(ellipse, 500, 500, color=0.15), angles=(0,90,45)) |> generate!
+wc = wordcloud(words, weights, 
+    mask=shape(ellipse, 500, 500, color=0.15),
+    density=0.5,
+    angles=(0,90,45)) |> generate!
