f119b549b026446bd69a0cea4db74125ea599bce
guoyongzhi <guoyongzhi@sensetime.com>
2021-05-14 17:04:32 +0800

    add example embedding

---
+++ examples/embedding.jl
@@ -0,0 +1,52 @@
+#md# The positions of words can be initialized with pre-trained word vectors.
+#md# ### Words
+using WordCloud
+stwords = ["us", "will"];
+words_weights = processtext(open(pkgdir(WordCloud)*"/res/Barack Obama's First Inaugural Address.txt"), stopwords=WordCloud.stopwords_en ∪ stwords)
+words_weights = Dict(zip(words_weights...))
+#md# ### Embeddings
+using Embeddings
+using TSne
+const embtable = load_embeddings(GloVe{:en})
+const get_word_index = Dict(word=>ii for (ii,word) in enumerate(embtable.vocab))
+function get_embedding(word)
+    ind = get_word_index[word]
+    emb = embtable.embeddings[:,ind]
+    return emb
+end
+wordvec = Dict()
+for k in keys(words_weights)
+    if k in keys(get_word_index)
+        wordvec[k] = get_embedding(k)
+    elseif lowercase(k) in keys(get_word_index)
+        wordvec[k] = get_embedding(lowercase(k))
+    else
+        pop!(words_weights, k)
+        println("remove ", k)
+    end
+end
+embedded = tsne(hcat(values(wordvec)...)', 2)
+#md# ### WordCloud
+sc = WordCloud.randomscheme()
+wc = wordcloud(
+    words_weights,
+    mask = shape(ellipse, 1000, 1000, backgroundcolor=(0,0,0,0), color=WordCloud.chooseabgcolor(sc)),
+    colors = sc,
+    run = initimages!
+)
+
+pos = embedded
+mean = sum(pos, dims=1) / size(pos, 1)
+r = maximum(sqrt.(pos[:,1].^2 + pos[:,2].^2 ))
+pos = (pos .- mean) ./ 2r
+sz = collect(size(wc.mask))'
+pos = round.(Int, pos .* sz .+ sz ./ 2)
+
+setpositions!(wc, keys(wordvec)|>collect, eachrow(pos), type=setcenter!)
+setstate!(wc, :placement!)
+generate!(wc, patient=-1)
+println("results are saved to embedding.png")
+paint(wc, "embedding.png")
+wc
+#eval# runexample(:embedding)
+#md# ![](embedding.png)  
