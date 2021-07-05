056737ef42ff21f2896727727d1809f1414ac738
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2021-07-06 02:00:52 +0800

    single temp file

--- src/artist.jl
+++ src/artist.jl
@@ -6,21 +6,29 @@ SerifFonts =["Baskerville", "Times New Roman", "華康儷金黑 Std", "華康儷
 macfonts = ["Trebuchet MS", "Helvetica", "Verdana", "Arial", "Comic Sans MS", "STHeiti", "Apple LiGothic", "Baskerville", "Times New Roman", "Hiragino Mincho Pro", "Georgia", "STSong", "STKaiti", "Courier New"]
 windowsfonts = ["Trebuchet MS", "sans-serif", "Verdana", "Arial", "Tahoma", "Microsoft Yahei", "Comic Sans MS", "Impact", "Segoe Script", "Segoe UI", "serif", "Georgia", "STSong", "FangSong", "KaiTi", "STKaiti", "Courier New"]
 CandiFonts = union(SansSerifFonts, SerifFonts)
-CandiWeights = ["", " bold", " light"]
-function checkfont(font)
+CandiWeights = ["", " regular", " normal", " medium", " bold", " light"]
+function checkfonts(fonts::AbstractVector)
     fname = tempname()
-    try
-        open(fname, "w") do f
-            redirect_stderr(f) do
-                rendertext("a", 4+rand(), font=font)
+    r = Bool[]
+    open(fname, "w") do f
+        redirect_stderr(f) do
+            p = position(f)
+            for font in fonts
+                rendertext("a", 1+rand(), font=font) #相同字体相同字号仅warning一次，故首次执行最准
+                #flush(f) #https://en.cppreference.com/w/cpp/io/c/fseek The standard C++ file streams guarantee both flushing and unshifting 
+                seekend(f)
+                p2 = position(f)
+                push!(r, p2 == p)
+                p = p2
             end
         end
-    finally
-        ret = isempty(read(fname, String))
-        rm(fname, force=true)
-        return ret
     end
+    return r
 end
-checkfonts(;fonts=CandiFonts, weights=CandiWeights) = ["$f$w" for w in weights, f in fonts if checkfont("$f$w")]
-AvailableFonts = checkfonts()
+checkfonts(f) = checkfonts([f]) |> only
+function filterfonts(;fonts=CandiFonts, weights=CandiWeights)
+    candi = ["$f$w" for w in weights, f in fonts] |> vec
+    candi[checkfonts(candi)]
+end
+AvailableFonts = filterfonts()
 push!(AvailableFonts, "")
