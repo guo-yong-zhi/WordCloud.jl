eb303e0fa83175394f30a4cfdbe5d7b0f7473d16
guo-yong-zhi <guo-yong-zhi@outlook.com>
2021-12-06 16:12:14 +0800

    add filter to animation1_placewords

--- examples/animation1_placewords.jl
+++ examples/animation1_placewords.jl
@@ -3,23 +3,20 @@ using WordCloud
 stopwords = WordCloud.stopwords_en ∪ ["said"]
 textfile = pkgdir(WordCloud)*"/res/alice.txt"
 wc = wordcloud(
-    processtext(open(textfile), stopwords=stopwords, maxnum=120), 
+    processtext(open(textfile), stopwords=stopwords, maxnum=300), 
     masksize = (300, 200),
     outline = 3,
     angles = 0:90,
-    density = 0.55,
     state = initwords!)
 #md# ### uniform style
 gifdirectory = "animation1_placewords/uniform"
-#eval# try rm("animation1_placewords/uniform", force=true, recursive=true) catch end 
 setpositions!(wc, :, (-1000,-1000))
-record(placewords!, wc, outputdir=gifdirectory, style=:uniform)
+record(placewords!, wc, style=:uniform, outputdir=gifdirectory, filter=i->i%(2^(i÷100))==0)
 #md# ![](animation1_placewords/uniform/result.gif)  
 #md# ### gathering style
 gifdirectory = "animation1_placewords/gathering"
-#eval# try rm("animation1_placewords/gathering", force=true, recursive=true) catch end 
 setpositions!(wc, :, (-1000,-1000))
-record(placewords!, wc, outputdir=gifdirectory, style=:gathering)
+record(placewords!, wc, style=:gathering, outputdir=gifdirectory, filter=i->i%(2^(i÷100))==0)
 #md# ![](animation1_placewords/gathering/result.gif)  
 #md# 
 println("results are saved in animation1_placewords")
