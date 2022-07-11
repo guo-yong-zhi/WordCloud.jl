dc9495dd5b3ba825319ee618fa14cacaf1a9e0d5
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2022-07-11 22:41:41 +0800

    plutoapp: append google_fonts to the AvailableFonts

--- plutoapp.jl
+++ plutoapp.jl
@@ -221,8 +221,20 @@ function gen_cloud(words_weights)
     return nothing
 end
 
+# ╔═╡ e7ec8cd7-f60b-4eb0-88fc-76d694976f9d
+begin
+google_fonts = ["Roboto","Open Sans","Lato","Montserrat","Noto Sans JP","Roboto Condensed","Oswald","Source Sans Pro","Slabo 27px","Raleway","PT Sans","Poppins","Roboto Slab","Merriweather","Noto Sans","Ubuntu","Roboto Mono","Lora","Playfair Display","Nunito","PT Serif","Titillium Web","PT Sans Narrow","Arimo","Noto Serif",
+"Rubik","Fira Sans","Work Sans","Noto Sans KR","Quicksand","Dosis","Inconsolata","Oxygen","Mukta","Bitter","Nanum Gothic","Yanone Kaffeesatz","Nunito Sans","Lobster","Cabin","Fjalla One","Indie Flower","Anton","Arvo","Josefin Sans","Karla","Libre Baskerville","Noto Sans TC","Hind","Crimson Text","Hind Siliguri",
+"Inter","Heebo","Abel","Libre Franklin","Barlow","Varela Round","Pacifico","Dancing Script","Exo 2","Source Code Pro","Shadows Into Light","Merriweather Sans","Asap","Bree Serif","Archivo Narrow","Play","Ubuntu Condensed","Questrial","Abril Fatface","Source Serif Pro","Maven Pro","Francois One","Signika",
+"EB Garamond","Comfortaa","Exo","Vollkorn","Teko","Catamaran","Kanit","Cairo","Amatic SC","IBM Plex Sans","Cuprum","Poiret One","Rokkitt","Bebas Neue","Acme","PT Sans Caption","Righteous","Noto Sans SC","Alegreya Sans","Alegreya","Barlow Condensed","Prompt","Gloria Hallelujah","Patua One","Crete Round","Permanent Marker"]
+empty!(WordCloud.AvailableFonts)
+append!(WordCloud.AvailableFonts, ["$f$w" for w in WordCloud.CandiWeights, f in google_fonts])
+nothing
+end
+
 # ╔═╡ fa6b3269-357e-4bf9-8514-70aff9df427f
 begin
+google_fonts #used to adjust cell order
 wc = gen_cloud(words_weights)
 wc
 end
@@ -262,3 +274,4 @@ end
 # ╟─0bf2ba32-321a-470d-8224-700cbc29cd7a
 # ╟─6a8d7068-2975-43ab-a387-7f0e7c2e4262
 # ╟─986cf1a6-8075-48ae-84d9-55ae11a27da1
+# ╟─e7ec8cd7-f60b-4eb0-88fc-76d694976f9d
