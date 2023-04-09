dbbdb36669d19fdd6d0862f278ebfb3b4005ba68
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2023-04-10 02:07:15 +0800

    plutoapp: support table input

--- plutoapp.jl
+++ plutoapp.jl
@@ -94,7 +94,68 @@ begin
     Tags are usually single words, and the importance of each tag is shown with font size or color. Bigger term means greater weight. 
     This format is useful for quickly perceiving the most prominent terms to determine its relative prominence.  
     """
-    nothing
+defaultttable = """
+				饥寒交迫, 1
+				落花流水, 1
+				受苦, 1
+				世界, 1
+				不要, 1
+				就, 2
+				为, 1
+				们, 1
+				一定, 2
+				团结起来, 2
+				，, 10
+				热血, 1
+				沸腾, 1
+				的, 6
+				而, 1
+				斗争, 3
+				最后, 2
+				真理, 1
+				人, 1
+				！, 7
+				奴隶, 2
+				旧, 1
+				主人, 1
+				这是, 2
+				英特纳雄耐尔, 2
+				说, 1
+				一无所有, 1
+				要, 4
+				已经, 1
+				我们, 2
+				天下, 1
+				实现, 2
+				起来, 4
+				全世界, 1
+				满腔, 1
+				打个, 1
+				做, 1
+				到, 2
+				明天, 2
+				"""
+nothing
+end
+
+# ╔═╡ 66f4b71e-01e5-4279-858b-04d44aeeb574
+begin
+function read_table(text)
+	ps = [split(it, r"[,;\t]") for it in split(strip(text), "\n")]
+	ps = sort([(first(it), parse(Float64, last(it))) for it in ps], by=last, rev=true)
+	maxlen = maximum(length∘first, ps)
+	println(length(ps), " items table:\n")
+	for (i,p) in enumerate(ps)
+		println("\t", p[1], " "^(maxlen-length(p[1]))*"\t|\t", p[end])
+		if i == 9
+			println("\t。。。")
+			break
+		end
+	end
+	println()
+	ps
+end
+nothing
 end
 
 # ╔═╡ 397fdd42-d2b2-46db-bf74-957909f47a58
@@ -211,15 +272,17 @@ nothing
 end
 
 # ╔═╡ bda3fa85-04a3-4033-9890-a5b4f10e2a77
-md"""$logo  **From** $(@bind texttype Select(["Text", "File", "Web"])) """
+md"""$logo  **From** $(@bind texttype Select(["Text", "File", "Web", "Table"])) """
 
 # ╔═╡ 9191230b-b72a-4707-b7cf-1a51c9cdb217
 if texttype == "Web"
     md"""**URL:** $(@bind url TextField(80, default="http://en.wikipedia.org/wiki/Special:random"))"""
 elseif texttype == "Text"
     @bind text_ TextField((80, 10), defaulttext)
-else
+elseif texttype == "File"
 	@bind uploadedfile FilePicker()
+else
+	@bind text_ TextField((30, 15), defaultttable)
 end
 
 # ╔═╡ d8e73850-f0a6-4170-be45-5a7527f1ec39
@@ -239,13 +302,15 @@ try
 		end
 	elseif texttype == "Text"
 		text = text_
-	else
+	elseif texttype == "File"
 		if uploadedfile !== nothing
 			text = read(IOBuffer(uploadedfile["data"]), String)
 		end
+	else
+		text = read_table(text_)
 	end
 	dict_process = rescaleweights(rescale_func, scalekeeptype) ∘ casemerge! ∘ lemmatize!
-	if ischinese(text)
+	if text isa AbstractString && ischinese(text)
 		println("检测到中文")
 		text = wordseg_cn(text)
 	end
@@ -357,6 +422,7 @@ end
 # ╟─2870a2ee-aa99-48ec-a26d-fed7b040e6de
 # ╟─fa6b3269-357e-4bf9-8514-70aff9df427f
 # ╟─21ba4b81-07aa-4828-875d-090e0b918c76
+# ╟─66f4b71e-01e5-4279-858b-04d44aeeb574
 # ╟─397fdd42-d2b2-46db-bf74-957909f47a58
 # ╟─74bd4779-c13c-4d16-a90d-597db21eaa39
 # ╟─9396cf96-d553-43db-a839-273fc9febd5a
