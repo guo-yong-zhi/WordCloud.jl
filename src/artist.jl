760ce658612120aa8dc13d43b6396b74ccbee32c
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2025-05-10 00:25:21 +0800

    format code

--- src/artist.jl
+++ src/artist.jl
@@ -2,7 +2,7 @@ using Random
 import Fontconfig: list, Pattern, format
 using StopWords
 
-const FONT_NAMES::Dict{String, Vector{String}} = Dict{String, Vector{String}}()
+const FONT_NAMES::Dict{String,Vector{String}} = Dict{String,Vector{String}}()
 const FONT_WEIGHTS::Vector{String} = ["", " Regular", " Normal", " Medium", " Bold", " Light"]
 
 function listfonts(lang="")
@@ -14,7 +14,7 @@ function listfonts(lang="")
     return [format(p, "%{family[0]}") for p in ps]
 end
 function reverse_dict(d)
-    rd = Dict{String, Vector{String}}()
+    rd = Dict{String,Vector{String}}()
     for (k, v) in d
         get!(rd, v) do
             String[]
@@ -62,13 +62,13 @@ end
 
 function getcolorschemes()
     schemes_colorbrewer = filter(s -> occursin("colorbrewer", colorschemes[s].category), collect(keys(colorschemes)))
-    schemes_colorbrewer =  filter(s -> (occursin("Accent", String(s)) 
-            || occursin("Dark", String(s))
-            || occursin("Paired", String(s))
-            || occursin("Pastel", String(s))
-            || occursin("Set", String(s))
-            || occursin("Spectral", String(s))
-            ), schemes_colorbrewer)
+    schemes_colorbrewer = filter(s -> (occursin("Accent", String(s))
+                                       || occursin("Dark", String(s))
+                                       || occursin("Paired", String(s))
+                                       || occursin("Pastel", String(s))
+                                       || occursin("Set", String(s))
+                                       || occursin("Spectral", String(s))
+        ), schemes_colorbrewer)
     schemes_seaborn = filter(s -> occursin("seaborn", colorschemes[s].category), collect(keys(colorschemes)))
     schemes_tableau = filter(s -> occursin("tableau", colorschemes[s].category), collect(keys(colorschemes)))
     schemes_cvd = filter(s -> occursin("cvd", colorschemes[s].category), collect(keys(colorschemes)))
@@ -85,19 +85,19 @@ function displayschemes()
         display(colors)
     end
 end
-function gradient(weights_or_num; colorscheme=rand(COLOR_SCHEMES), section=(0,1))
+function gradient(weights_or_num; colorscheme=rand(COLOR_SCHEMES), section=(0, 1))
     @assert length(section) == 2
-    a,b = section
+    a, b = section
     @assert a <= b
     C = Render.colorschemes[colorscheme]
     if weights_or_num isa Number
         inds = range(a, b, length=max(2, weights_or_num))
     else
         weights = float.(weights_or_num)
-        length(weights) < 2 && (weights = [1.,1.])
-        weights[1] = 0.
+        length(weights) < 2 && (weights = [1.0, 1.0])
+        weights[1] = 0.0
         cumsum!(weights, weights)
-        weights[end] != 0. && (weights ./= weights[end])
+        weights[end] != 0.0 && (weights ./= weights[end])
         weights .*= b - a
         weights .+= a
         inds = weights
@@ -116,7 +116,7 @@ function randomcolorscheme(weights_or_num=100)
         else
             if rand() < 0.3
                 a, b = minmax(rand(1:lastindex(C)), rand(1:lastindex(C)))
-                b - a < length(C)÷10 && (a = 1; b = lastindex(C))
+                b - a < length(C) ÷ 10 && (a = 1; b = lastindex(C))
                 rg = range(a, b; step=1)
                 print("color scheme: ", repr(scheme), ", random range: $a:$b")
                 rand() > 0.5 && (rg = reverse(rg); print(", reversed"))
@@ -132,9 +132,9 @@ function randomcolorscheme(weights_or_num=100)
                     print(", weight based gradient")
                 end
                 a, b = round.(minmax(rand(), rand()), digits=3)
-                rand() < 0.2 && (a = 0.; b = 1.)
+                rand() < 0.2 && (a = 0.0; b = 1.0)
                 print(", random section: $a:$b")
-                colors = gradient(weights_or_num; colorscheme=scheme, section=(a,b))
+                colors = gradient(weights_or_num; colorscheme=scheme, section=(a, b))
             end
         end
         if rand() > 0.5
@@ -158,7 +158,7 @@ end
 # end
 function randomwh(sz::Number=800)
     s = sz * sz
-    ratio = (9/16 + rand()*7/16)
+    ratio = (9 / 16 + rand() * 7 / 16)
     ratio > 0.9 && (ratio = 1.0)
     h = round(Int, sqrt(s * ratio))
     w = round(Int, h / ratio)
@@ -170,8 +170,8 @@ equalwh(sz::Number=800) = sz, sz
 equalwh(sz::Tuple) = sz
 equalwh(arg...) = arg
 function randommask(args...; maskshape=:rand, returnkwargs=false, kargs...)
-    rd = Dict(squircle => 0.4, box => 0.6, ellipse => 0.8, 
-    ngon => 0.85, star => 0.9, bezingon => 0.92, bezistar => 1)
+    rd = Dict(squircle => 0.4, box => 0.6, ellipse => 0.8,
+        ngon => 0.85, star => 0.9, bezingon => 0.92, bezistar => 1)
     if maskshape ∉ keys(rd) && maskshape isa Function
         s, k = maskshape(args...; kargs...), kargs
     else
@@ -202,13 +202,15 @@ end
 function randombox(w, h; cornerradius=:rand, preservevolume=false, kargs...)
     if cornerradius == :rand
         r = rand() * 0.5 - 0.05 # up to 0.45
-        r < 0. && (r = 0.) # 10% for 0.
+        r < 0.0 && (r = 0.0) # 10% for 0.
         r = round(Int, h * r)
     else
         r = cornerradius
     end
-    sc = preservevolume ? sqrt(w*h/box_area(w, h, cornerradius=r)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc); r = round(Int, r*sc)
+    sc = preservevolume ? sqrt(w * h / box_area(w, h, cornerradius=r)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
+    r = round(Int, r * sc)
     return callshape(box, w, h; cornerradius=r, kargs...)
 end
 function randomsquircle(w, h; rt=:rand, preservevolume=false, kargs...)
@@ -225,20 +227,22 @@ function randomsquircle(w, h; rt=:rand, preservevolume=false, kargs...)
         end
         rt = round(rt, digits=3)
     end
-    sc = preservevolume ? sqrt(w*h/squircle_area(w, h, rt=rt)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc)
+    sc = preservevolume ? sqrt(w * h / squircle_area(w, h, rt=rt)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
     return callshape(squircle, w, h, rt=rt; kargs...)
 end
 function randomellipse(w, h; preservevolume=false, kargs...)
-    sc = preservevolume ? sqrt(w*h/ellipse_area(w, h)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc)
+    sc = preservevolume ? sqrt(w * h / ellipse_area(w, h)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
     return callshape(ellipse, w, h; kargs...)
 end
 function randomorientation(n)
     if n == 3
-        ori = rand((0, π/2, π/3))
+        ori = rand((0, π / 2, π / 3))
     elseif n % 2 == 0
-        ori = rand((0, π/n))
+        ori = rand((0, π / n))
     else
         ori = 0
     end
@@ -247,37 +251,41 @@ end
 function randomngon(w, h; npoints=:rand, orientation=:rand, preservevolume=false, kargs...)
     npoints == :rand && (npoints = rand(3:12))
     orientation == :rand && (orientation = randomorientation(npoints))
-    sc = preservevolume ? sqrt(w*h/ngon_area(w, h, npoints=npoints)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc)
+    sc = preservevolume ? sqrt(w * h / ngon_area(w, h, npoints=npoints)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
     return callshape(ngon, w, h; npoints=npoints, orientation=orientation, kargs...)
 end
 function randomstar(w, h; npoints=:rand, starratio=:rand, orientation=:rand, preservevolume=false, kargs...)
     npoints == :rand && (npoints = rand(5:12))
     orientation == :rand && (orientation = randomorientation(npoints))
     if starratio == :rand
-        starratio = cos(π/npoints) * (0.7 + 0.25rand())
+        starratio = cos(π / npoints) * (0.7 + 0.25rand())
         starratio = round(starratio, digits=3)
     end
-    sc = preservevolume ? sqrt(w*h/star_area(w, h, npoints=npoints, starratio=starratio)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc)
+    sc = preservevolume ? sqrt(w * h / star_area(w, h, npoints=npoints, starratio=starratio)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
     return callshape(star, w, h; npoints=npoints, starratio=starratio, orientation=orientation, kargs...)
 end
 function randombezingon(w, h; npoints=:rand, orientation=:rand, preservevolume=false, kargs...)
-    npoints == :rand && (npoints = rand((3,3,4)))
+    npoints == :rand && (npoints = rand((3, 3, 4)))
     orientation == :rand && (orientation = randomorientation(npoints))
-    sc = preservevolume ? sqrt(w*h/ngon_area(w, h, npoints=npoints)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc)
+    sc = preservevolume ? sqrt(w * h / ngon_area(w, h, npoints=npoints)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
     return callshape(bezingon, w, h; npoints=npoints, orientation=orientation, kargs...)
 end
 function randombezistar(w, h; npoints=:rand, starratio=:rand, orientation=:rand, preservevolume=false, kargs...)
     npoints == :rand && (npoints = rand(3:12))
     orientation == :rand && (orientation = randomorientation(npoints))
     if starratio == :rand
-        starratio = cos(π/npoints) * (0.7 + 0.25rand())
+        starratio = cos(π / npoints) * (0.7 + 0.25rand())
         starratio = round(starratio, digits=3)
     end
-    sc = preservevolume ? sqrt(w*h/star_area(w, h, npoints=npoints, starratio=starratio)) : 1
-    w = round(Int, w*sc); h = round(Int, h*sc)
+    sc = preservevolume ? sqrt(w * h / star_area(w, h, npoints=npoints, starratio=starratio)) : 1
+    w = round(Int, w * sc)
+    h = round(Int, h * sc)
     return callshape(bezistar, w, h; npoints=npoints, starratio=starratio, orientation=orientation, kargs...)
 end
 function randomangles()
@@ -286,7 +294,7 @@ function randomangles()
     angles = rand((0, (0, 90), (0, 45, 90), (0, 45, 90, -45), -90:90, -90:st:90,
         -5:5, (0, θ, -θ), (θ, -θ), -θ:θ, -θ:st:θ))
     if length(angles) > 1 && rand() > 0.5
-        0 in angles && maximum(abs, angles)>10 && (angles = angles .- first(angles))
+        0 in angles && maximum(abs, angles) > 10 && (angles = angles .- first(angles))
         if angles isa Tuple
             angles = collect(angles)
             println("angles = ", angles)
@@ -295,7 +303,7 @@ function randomangles()
             angles = collect(angles)
         end
     else
-        rand() > 0.7 && (angles =  -1 .* angles)
+        rand() > 0.7 && (angles = -1 .* angles)
         println("angles = ", angles)
     end
     angles
@@ -316,9 +324,9 @@ function randommaskcolor(colors)
         end
         # @show I, m, M
         if I > 3(1 - M) && I > 3m
-            middle = (g[i] + g[i + 1]) / 2
+            middle = (g[i] + g[i+1]) / 2
             th1 = clamp(max(g[i] + 0.15, middle - rand(0:0.001:0.05)), 0, middle)
-            th2 = clamp(min(g[i + 1] - 0.15, middle + rand(0:0.001:0.05)), middle, 1)
+            th2 = clamp(min(g[i+1] - 0.15, middle + rand(0:0.001:0.05)), middle, 1)
             default = middle
         elseif sum(g) / length(g) < 0.7 && (m + M) / 2 < 0.7 && !(m > 2(1 - M))# 明亮
             th1 = clamp(max(M + 0.15, rand(0.85:0.001:1.0)), 0, 1)
@@ -329,15 +337,15 @@ function randommaskcolor(colors)
             th1 = clamp(th2 - 0.15, 0, 1)
             default = 0.0
         end
-        maskcolor = rand((default, 
-        (round(rand(th1:0.001:th2), digits=3),
-        round(rand(th1:0.001:th2), digits=3),
-        round(rand(th1:0.001:th2), digits=3))))
+        maskcolor = rand((default,
+            (round(rand(th1:0.001:th2), digits=3),
+                round(rand(th1:0.001:th2), digits=3),
+                round(rand(th1:0.001:th2), digits=3))))
         # @show maskcolor
         return maskcolor
     catch e
         @show e
-    @show "colors sum failed", colors
+        @show "colors sum failed", colors
         return "white"
     end
 end
@@ -346,10 +354,10 @@ function randomlinecolor(colors)
         linecolor = rand((colors[1], colors[1], rand(colors)))
     else
         linecolor = (
-            round(rand(), digits=3), 
-            round(rand(), digits=3), 
-            round(rand(), digits=3), 
-            min(1., round(0.5 + rand()/2, digits=3)))
+            round(rand(), digits=3),
+            round(rand(), digits=3),
+            round(rand(), digits=3),
+            min(1.0, round(0.5 + rand() / 2, digits=3)))
     end
     linecolor
 end
